b272b37b7b51977c73863318d7f4af58ab0675b9
AnetN <ndwigaannetr13@gmail.com>
2022-07-06 01:26:52 -0700

    ind

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,17 +2,15 @@
 /* printing the alphabet */
 int main(void)
 {
-	char c;
+char c;
+     for (c = 'a'; c <= 'z'; c++)
 
-	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+     for (c = 'A'; c <= 'Z'; c++)
 
-		putchar(c);
+	putchar(c);
 
-	for (c = 'A'; c <= 'Z'; c++)
-
-		putchar(c);
-
-		putchar('\n');
+	putchar('\n');
 
 	return (0);
 }
