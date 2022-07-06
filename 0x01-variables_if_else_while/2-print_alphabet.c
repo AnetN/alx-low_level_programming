5f8c99dbb879471809f718ce7b2c425b31353e3a
AnetN <ndwigaannetr13@gmail.com>
2022-07-06 02:18:22 -0700

    error

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
-/* printing the alphabet */
-int main(void)
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+ int main(void)
+{
+ char c;
 
-{ char c;
+ for (c = 'a'; c <= 'z'; c++)
 
-for (c = 'a'; c <= 'z'; c++)
+ putchar(c);
+ putchar('\n');
 
-putchar(c);
-
-
-
-for (c = 'A'; c <= 'Z'; c++)
-
-putchar(c);
-
-     return (0);
-
+ return (0);
 }
