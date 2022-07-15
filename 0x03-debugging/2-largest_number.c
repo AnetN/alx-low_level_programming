9b8a935d79c958437159099638738044de13a3cf
AnetN <ndwigaannetr13@gmail.com>
2022-07-15 02:33:44 -0700

    :mew

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -7,16 +7,16 @@
  * Return: largest number
  */
 int largest_number(int a, int b, int c)
-{
-  int largest;
+{int largest;
 
-  if (a > b && a > c)
+if (a > b && a > c)
 {
 largest = a;
 }
 else if (b > c)
 {
 largest = b;
+
 }
 else
 {
